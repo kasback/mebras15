4edf5b8078bbfc3737ddc374ffc620111a26a4a9
kasback <kasbajinassrallah@gmail.com>
2023-05-25 10:52:30 +0100

    [FIX] tax fix

--- l10n_maroc/__manifest__.py
+++ l10n_maroc/__manifest__.py
@@ -2,13 +2,14 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Morocco - Accounting',
+    'name': 'Données de base de la comptabilité Marocaine',
     'version': '1.0',
     'category': 'Accounting/Localizations/Account Charts',
     'description': """Plan comptable, Types de comptes, Taxes""",
-
+    'summary': """
+        Plan comptable, Types de comptes, Taxes""",
     'author': 'Osisoftware',
-    'website': 'http://www.osisoftware.com',
+    'website': 'https://osisoftware.net/',
     'depends': ['account', 'account_tax_code'],
     'data': [
         'data/account_chart_template.xml',
@@ -17,8 +18,9 @@
         'data/account.account.template.csv',
         'data/property_account.xml',
         'data/account.tax.template.csv',
-        'data/account.chart.template.csv',
         'data/account_chart_template_configuration_data.xml',
+        'views/res_company.xml',
+        'views/res_config.xml',
         'security/account_tres_security.xml',
     ],
     'demo': [
