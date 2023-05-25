b13772975557dc18680ea956e5784f6e3d5c2300
kasback <kasbajinassrallah@gmail.com>
2023-05-25 10:56:37 +0100

    [FIX] tax fix

--- l10n_maroc/__manifest__.py
+++ l10n_maroc/__manifest__.py
@@ -2,14 +2,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Données de base de la comptabilité Marocaine',
+    'name': 'Morocco - Accounting',
     'version': '1.0',
     'category': 'Accounting/Localizations/Account Charts',
     'description': """Plan comptable, Types de comptes, Taxes""",
-    'summary': """
-        Plan comptable, Types de comptes, Taxes""",
+
     'author': 'Osisoftware',
-    'website': 'https://osisoftware.net/',
+    'website': 'http://www.osisoftware.com',
     'depends': ['account', 'account_tax_code'],
     'data': [
         'data/account_chart_template.xml',
@@ -18,9 +17,8 @@
         'data/account.account.template.csv',
         'data/property_account.xml',
         'data/account.tax.template.csv',
+        'data/account.chart.template.csv',
         'data/account_chart_template_configuration_data.xml',
-        'views/res_company.xml',
-        'views/res_config.xml',
         'security/account_tres_security.xml',
     ],
     'demo': [
--- l10n_maroc/models/account.py
+++ l10n_maroc/models/account.py
@@ -6,13 +6,13 @@ from odoo import models, fields, api
 class Account(models.Model):
     _inherit = "account.account"
 
-    # @api.model
-    # def create(self, vals):
-    #     print('vals', vals['group_id'])
-    #     if not vals['group_id']:
-    #         digits = vals['code'][0:3]
-    #         group_id = self.env['account.group'].search([('code_prefix_start', '=', digits)])
-    #         vals['group_id'] = group_id.id
-    #     res = super(Account, self).create(vals)
+    @api.model
+    def create(self, vals):
+        print('vals', vals['group_id'])
+        if not vals['group_id']:
+            digits = vals['code'][0:3]
+            group_id = self.env['account.group'].search([('code_prefix_start', '=', digits)])
+            vals['group_id'] = group_id.id
+        res = super(Account, self).create(vals)
 
-    #     return res
+        return res
